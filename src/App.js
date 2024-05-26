914ab11f034c8218682e808bebcfb70999f9b307
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 <sharath.yaragatti@yahoo.com>
2024-05-26 23:41:09 +0530

    clock app first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <div className='wrapper'>
+             <div className='heading'>
+                 <h2>25 + 5 Clock</h2>
+             </div>
+             <div className='sub-wrap'>
+                 <div className='labels' id='break-label'>
+                      <h3>Break Length</h3>
+                      <div className='sub-box'>
+                      <div id='break-decrement'>symbol</div>
+                      <div id='break-length'>5</div>
+                      <div id='break-increment'>symbol</div>
+                      </div>
+                  </div>
+                 <div className='labels' id='session-label'>
+                      <h3>Session Length</h3>
+                      <div className='sub-box'>
+                      <div id='session-decrement'>symbol</div>
+                      <div id='session-length'>25</div>
+                      <div id='session-increment'>symbol</div>
+                      </div>
+                  </div>
+             </div>
+             <div className='timer-box'>
+                <div id='timer-label'>Session</div>
+                <div id='time-left'>25:00</div>
+             </div>
+             <div className='controls'>
+                <div id='start_stop'>start/pause</div>
+                <div id='reset'>reset</div>
+             </div>
+             <div className='credits'>
+                <h4>Designed and Coded by</h4>
+                <h4>Sharat Yaragatti</h4>
+             </div>
+         </div>        
     </div>
   );
 }
