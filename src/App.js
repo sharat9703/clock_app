08c29b81cb481e9f08236d2a677e6479e164c546
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 <sharath.yaragatti@yahoo.com>
2024-05-27 13:14:24 +0530

    fontAwesome icons added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowDown,faArrowUp,faPlay,faStop,faRepeat }  from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faArrowDown,faArrowUp,faPlay,faRepeat,faStop);
 
+const arrow_up = <FontAwesomeIcon icon={faArrowUp}/>
+const arrow_down = <FontAwesomeIcon icon={faArrowDown}/>
+const icon_start =<FontAwesomeIcon icon={faPlay}/>
+const icon_stop = <FontAwesomeIcon icon={faStop}/>
+const icon_reset = <FontAwesomeIcon icon={faRepeat}/>
 function App() {
   return (
     <div className="App">
@@ -11,27 +20,27 @@ function App() {
                  <div className='labels' id='break-label'>
                       <h3>Break Length</h3>
                       <div className='sub-box'>
-                      <div id='break-decrement'>symbol</div>
-                      <div id='break-length'>5</div>
-                      <div id='break-increment'>symbol</div>
+                      <div className='sub-el' id='break-decrement'>{arrow_down}</div>
+                      <div className='sub-el' id='break-length'>5</div>
+                      <div className='sub-el' id='break-increment'>{arrow_up}</div>
                       </div>
                   </div>
                  <div className='labels' id='session-label'>
                       <h3>Session Length</h3>
                       <div className='sub-box'>
-                      <div id='session-decrement'>symbol</div>
-                      <div id='session-length'>25</div>
-                      <div id='session-increment'>symbol</div>
+                      <div className='sub-el' id='session-decrement'>{arrow_down}</div>
+                      <div className='sub-el' id='session-length'>25</div>
+                      <div className='sub-el' id='session-increment'>{arrow_up}</div>
                       </div>
                   </div>
              </div>
              <div className='timer-box'>
-                <div id='timer-label'>Session</div>
+                <h3 id='timer-label'>Session</h3>
                 <div id='time-left'>25:00</div>
              </div>
              <div className='controls'>
-                <div id='start_stop'>start/pause</div>
-                <div id='reset'>reset</div>
+                <div id='start_stop'>{icon_start}{icon_stop}</div>
+                <div id='reset'>{icon_reset}</div>
              </div>
              <div className='credits'>
                 <h4>Designed and Coded by</h4>
